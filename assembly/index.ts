81718d89577f5ed8499095329252e42047f3bea7
jakewmiles <jmiles@moonpay.com>
2022-10-04 20:54:40 +0100

    feat: implement buyProduct smart contract call

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,4 +1,5 @@
 import { Product, listedProducts } from "./model";
+import { ContractPromiseBatch, context } from "near-sdk-core";
 
 export function setProduct(product: Product): void {
   let storedProduct = listedProducts.get(product.id);
@@ -14,4 +15,17 @@ export function getProduct(id: string): Product | null {
 
 export function getProducts(): Product[] {
   return listedProducts.values();
+}
+
+export function buyProduct(productId: string): void {
+  const product = getProduct(productId);
+  if (product == null) {
+    throw new Error("product not found!");
+  }
+  if (product.price.toString() != context.attachedDeposit.toString()) {
+    throw new Error("attached deposit should equal the product's price!");
+  }
+  ContractPromiseBatch.create(product.owner).transfer(context.attachedDeposit);
+  product.incrementSoldAmount();
+  listedProducts.set(product.id, product);
 }
